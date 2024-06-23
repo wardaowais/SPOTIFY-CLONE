7fa80ae52f812312b34eb92c4f3104faa42b044a
wardaowais <wardaowais123@gmail.com>
2024-06-23 19:07:20 +0500

    this is first commit

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function secondsToMinutesSeconds(seconds) {
 //function to fetch all the songs from folder
 async function getSongs(folder) {
     // console.log("this ia folder",folder)
-    let a = await fetch(`http://127.0.0.1:5500/${folder}/`)
+    let a = await fetch(`/${folder}/`)
     currFolder = folder;
     let response = await a.text();
     // console.log("this is response",response)
@@ -70,8 +70,8 @@ async function getSongs(folder) {
 
 }
 function playMusic(track, pause = false) {
-    // let audio = new Audio("http://127.0.0.1:5500/songs/" + track)
-    currentSong.src = `http://127.0.0.1:5500/${currFolder}/` + track
+    // let audio = new Audio("/songs/" + track)
+    currentSong.src = `/${currFolder}/` + track
     if (!pause) {
         currentSong.play()
         play.src = "img/pause.svg"
@@ -82,7 +82,7 @@ function playMusic(track, pause = false) {
 }
 
 async function displayAlbums() {
-    let a = await fetch(`http://127.0.0.1:5500/songs/`)
+    let a = await fetch(`/songs/`)
     let response = await a.text();
     console.log("cards showing",response)
     let div = document.createElement("div")
@@ -96,7 +96,7 @@ async function displayAlbums() {
         if( (e.href.includes("/songs/"))  && !e.href.includes(".htaccess")){
             let folder = e.href.split("/").slice(-1)[0]
             //get the meta data of the folder
-            let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`)
+            let a = await fetch(`/songs/${folder}/info.json`)
             let response = await a.json();
             console.log(response)
             cardContainer.innerHTML = cardContainer.innerHTML + `<div  data-folder="${folder}"  class="card ">
